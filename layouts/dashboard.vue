ca272a92f92ef05acc91faaaedf347bab0950bef
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-07-02 04:18:20 -0300

    Criado pagina principal

---
+++ layouts/dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container-dashboard">
+    <Header />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue"
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+.container-dashboard {
+  display: flex;
+}
+</style>
