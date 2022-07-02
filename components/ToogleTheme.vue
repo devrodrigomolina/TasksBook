57a08f31ad105cf4841bb1474e7d1433c41c025a
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-07-02 04:20:03 -0300

    Inicio do botão de alterar tema

---
+++ components/ToogleTheme.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container-toogle">
+    <div class="item-toogle">
+      <fa class="fa-2xl" icon="sun" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.item-toogle {
+
+  color: white;
+}
+</style>
