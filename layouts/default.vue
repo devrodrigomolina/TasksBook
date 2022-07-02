74b88357c078f44ca407bc75436eb1403581efaf
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-07-02 04:18:50 -0300

    Criado pagina default

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="default-layout">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="error-layout">
+    <img class="image-error" src="@/static/error.jpg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.error-layout {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: black;
+}
+.image-error {
+  width: 80vw;
+  height: 100vh;
+}
+
+</style>
