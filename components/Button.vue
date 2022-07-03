ded05a94f7c2d21abfd736aa1d8d6e342dee7d6e
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-07-03 03:15:23 -0300

    Funcionalidade botão addtask

--- components/Button.vue
+++ components/Button.vue
@@ -1,20 +1,40 @@
 <template>
   <div class="container-btn">
-    <button class="btn"><fa class="icon fa-xl" icon="circle-plus" />Nova Task </button>
+
+    <button @click="newTask = !newTask" class="btn"><fa class="icon fa-xl" icon="circle-plus" />Nova Task </button>
+    <div v-if="newTask" class="ipt-new-task">
+      <input v-model="taskText" placeholder="Digite aqui sua tarefa" type="text" name="" id="">
+        <div class="none icons">
+          <fa @click="completeTask" class="complete-item" icon="check" /> 
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
+
 export default {
+  data() {
+    return {
+      newTask: false,
+      taskText: '',
+    }
+  },
+  methods: {
+    completeTask() {
+      this.$store.dispatch('addTask', this.taskText)
+    }
+  }
 
 }
 </script>
 
-<style>
+<style scoped>
 .container-btn {
   display: inline-block;
   width: 220px;
   height: 40px;
+  position: relative;
 }
 .btn {
   width: 100%;
@@ -30,5 +50,28 @@ export default {
 .btn .icon {
   color: white;
   margin-right: 10px;
+}
+
+.ipt-new-task {
+  width: 100%;
+  display: flex;
+  position: relative;
+}
+
+.ipt-new-task input {
+  width: 100%;
+  height: 30px;
+  outline: none;
+  padding-left: 8px;
+  border-radius: 10px;
+}
+.icons {
+  position: absolute;
+  right: 10px;
+  top: 5px;
+}
+.complete-item {
+  cursor: pointer;
+  color: green;
 }
 </style>
