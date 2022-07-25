5921421b21d5c2e68cf057d8c9a0dd78c81bc0b4
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-07-24 22:43:49 -0300

    Inicio da implementação de troca de tema

--- components/ToogleTheme.vue
+++ components/ToogleTheme.vue
@@ -1,21 +1,34 @@
 <template>
   <div class="container-toogle">
     <div class="item-toogle">
-      <fa class="fa-2xl" icon="sun" />
+      <fa @click="changeTheme" class="fa-2xl" icon="sun" />
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    changeTheme() {
+      document.body.classList.toggle('clear')
+      document.querySelector('.menu').classList.toggle('white-clear')
+      document.querySelector('.container-tasks-infos').classList.toggle('white-clear')
+    }
+  }
 }
 </script>
 
 <style>
-
+.clear {
+  background: #FAFAFA;
+}
+.white-clear {
+  background: #FFFF;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.322);
+}
 .item-toogle {
-
+  position: relative;
+  z-index: 100;
   color: white;
 }
 </style>
